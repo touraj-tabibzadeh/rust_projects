b77a700541e52763a4878e610a08254be66a3403
touraj tabibzadeh <tourajtabibzadeh@gmail.com>
2024-07-21 14:58:51 +0330

    calculator-for

--- 100-exercises-to-learn-rust-main/exercises/02_basic_calculator/07_for/src/lib.rs
+++ 100-exercises-to-learn-rust-main/exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,11 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n==1 || n==0 {return 1;}
+    let mut result = n;
+    for i in 1..n {
+        result = result*i;
+    }
+    result
 }
 
 #[cfg(test)]
