b1330c3b140c0810077f6026d0dc4ab4be1a7dc6
touraj tabibzadeh <tourajtabibzadeh@gmail.com>
2024-08-17 13:53:32 +0330

    struct 01

--- 100-exercises-to-learn-rust-main/exercises/03_ticket_v1/01_struct/src/lib.rs
+++ 100-exercises-to-learn-rust-main/exercises/03_ticket_v1/01_struct/src/lib.rs
@@ -1,9 +1,23 @@
+use std::arch::x86_64::_CMP_FALSE_OQ;
+
 // Define a struct named `Order` with the following fields:
 // - `price`, an unsigned integer
 // - `quantity`, an unsigned integer
-//
+struct Order {
+    price:u32,
+    quantity:u32,
+}
 // It should also have a method named `is_available` that returns a `true` if the quantity is
 // greater than 0, otherwise `false`.
+impl Order {
+fn is_available (self)->bool{
+    if self.quantity>0 {
+        true
+    } else {
+    false
+    }
+}
+}
 
 #[cfg(test)]
 mod tests {
