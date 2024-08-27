4f2be45dd97f3162b420c3b830febdb427fa30af
touraj tabibzadeh <tourajtabibzadeh@gmail.com>
2024-08-27 16:08:25 +0330

    setters

--- 100-exercises-to-learn-rust-main/exercises/03_ticket_v1/07_setters/src/lib.rs
+++ 100-exercises-to-learn-rust-main/exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -10,7 +10,10 @@ pub struct Ticket {
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
+
+    //validation        
+
+     fn validate(title:&String,description:&String,status:&String){
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
@@ -26,7 +29,12 @@ impl Ticket {
         if status != "To-Do" && status != "In Progress" && status != "Done" {
             panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
         }
+    }
 
+    //constructor
+    pub fn new(title: String, description: String, status: String) -> Ticket {
+        
+        Self::validate(&title,&description,&status);
         Ticket {
             title,
             description,
@@ -34,6 +42,7 @@ impl Ticket {
         }
     }
 
+    //getters
     pub fn title(&self) -> &String {
         &self.title
     }
@@ -44,6 +53,20 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    //setters
+    pub fn set_title(&mut self, new_title: String) {
+        Self::validate(&new_title, &self.description, &self.status);
+        self.title = new_title;
+    }
+    pub fn set_description(&mut self, new_description: String) {
+        Self::validate(&self.title, &new_description, &self.status);
+        self.description = new_description;
+    }
+    pub fn set_status(&mut self, new_status: String) {
+        Self::validate(&self.title, &self.description,&new_status);
+        self.status = new_status;
     }
 }
 
